5a3e5104647f8af288a326774dcd147cfb2a8c5e
piaoyizy <925754@qq.com>
2024-12-05 23:04:47 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,13 +8,30 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-# fw876/helloworld
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.defaultault
+echo "=============================================================================="
+echo "Freeing up disk space on CI system"
+echo "=============================================================================="
 
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo "Listing 100 largest packages"
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n | tail -n 100
+df -h
+echo "Removing large packages"
+sudo apt-get remove -y '^dotnet-.*'
+sudo apt-get remove -y '^llvm-.*'
+sudo apt-get remove -y 'php.*'
+sudo apt-get remove -y '^mongodb-.*'
+sudo apt-get remove -y '^mysql-.*'
+sudo apt-get remove -y azure-cli google-cloud-sdk hhvm google-chrome-stable firefox powershell mono-devel
+sudo apt-get autoremove -y
+sudo apt-get clean
+df -h
+echo "Removing large directories"
 
-#sed -i 's@coolsnowwolf/packages@P3TERX/packages@' feeds.conf.default
-
-#sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-
-#sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+sudo rm -rf /usr/share/dotnet/
+sudo rm -rf /usr/local/graalvm/
+sudo rm -rf /usr/local/.ghcup/
+sudo rm -rf /usr/local/share/powershell
+sudo rm -rf /usr/local/share/chromium
+sudo rm -rf /usr/local/lib/android
+sudo rm -rf /usr/local/lib/node_modules
+df -h
