c59eac1d0994b744234dbf48576c6b5d2107c645
piaoyizy <925754@qq.com>
2025-01-09 21:48:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,6 +8,17 @@
 # Blog: https://p3terx.com
 #============================================================
 
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+rm -rf package/feeds/luci/luci-app-openclash/
+git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
 # Modify default IP
 sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
 
